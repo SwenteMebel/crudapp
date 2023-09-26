210538e17c655ea04fc82ce145e20f5f2ee765e7
Swen <swentemebel@outlook.com>
2023-09-26 16:07:52 +0200

    klaar maken voor db connectie

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>CRUD-Applicatie</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Styles-->
+    <link rel='stylesheet' href="css/style.css">
+    </head>
+    <body>
+
+    <!--Header-->
+    <?php include_once "header.php";?>
+ 
+
+
+    </body>
+</html>
