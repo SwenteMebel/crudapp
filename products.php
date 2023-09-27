1d8f79a31549dfa032f5192a8383180bb77a0660
Swen <swentemebel@outlook.com>
2023-09-27 13:50:08 +0200

    Add customize product php page

--- products.php
+++ products.php
@@ -12,8 +12,26 @@
 
     <!--Header-->
     <?php include_once "header.php";?>
-    
-    
 
+
+    <!---->
+    <div class="addform">
+        <h3>Add Product</h3>
+        <form action='addProduct.cont.php' method='post'>
+            <input type='text' name='addProduct' placeholder='Name...'>
+            <input type='number' name='price' placeholder="Price..." min=0 step=0.100>
+            <input type='submit' value='Add Product'>
+        </form>
+    </div>
+
+    <div class="custform">
+        <h3>Customize Product</h3>
+        <form action='custProduct.cont.php' method='post'>
+            <input type='text' name='costProduct' placeholder='Name...'>
+            <input type='number' name='costPrice' placeholder="Price..." min=0 step=0.100>
+            <input type='submit' value='Change Product'>
+        </form>
+    </div>
+        
     </body>
 </html>
